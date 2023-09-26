4d44369c74c0bedbf5b51f5ce41760d74c398a63
Vic1696 <victormwenda39@gmail.com>
2023-09-26 10:45:18 +0300

    Singly linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,15 @@
  * Return: the number of elements
  *
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-            count++;
-    }
-    return (count);
+	while (h != NULL)
+	{
+	h = h->next;
+		count++;
+	}
+	return (count);
 }
